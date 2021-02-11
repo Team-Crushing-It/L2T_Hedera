7cd9c8f3e3f25398b8e3188a5904641dba0f1524
hiwa-jalal <hiwadoski21@gmail.com>
2021-02-11 21:11:49 +0300

    completed auth

---
+++ l2t_hedera/lib/app.dart
@@ -0,0 +1,48 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:l2t_hedera/sign_up/view/sign_up_page.dart';
+
+import 'auth/bloc/authentication_bloc.dart';
+import 'login/view/login_page.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key key,
+    @required this.authenticationRepository,
+  })  : assert(authenticationRepository != null),
+        super(key: key);
+
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
