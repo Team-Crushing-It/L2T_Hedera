5e3b35781f14680d20533413fa1716dd1e4f690d
hiwa-jalal <hiwadoski21@gmail.com>
2021-03-02 20:20:16 +0300

    added authentication test

--- l2t_hedera/lib/navbar/view/navbar.dart
+++ l2t_hedera/lib/navbar/view/navbar.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 
 class Navbar extends AppBar {
   Widget build(BuildContext context) {
+    return _buildHomeAppBarWeb();
+  }
+
+  AppBar _buildHomeAppBarWeb() {
     return AppBar(
-      title: Text('L2T: Learn to Teach, Teach to Learn'),
+      backgroundColor: Color(0xffFFD700),
+      title: Row(
+        children: [
+          const Text(
+            'L2T',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w900,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(width: 10),
+          CircleAvatar(child: Icon(Icons.person), radius: 15),
+          SizedBox(width: 10),
+          Icon(Icons.inbox, color: Colors.black),
+          SizedBox(width: 10),
+          Icon(Icons.emoji_events, color: Colors.black),
+          SizedBox(width: 10),
+          Icon(Icons.live_help, color: Colors.black),
+        ],
+      ),
     );
   }
 }
